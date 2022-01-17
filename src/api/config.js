442b0e0ef604c5e3dd666f5d6262f73c2384edd0
Keith Chen <chen.zhun@anyteach.cn>
2022-01-17 14:23:48 +0800

    chore: API 本地代理

--- src/api/config.js
+++ src/api/config.js
@@ -1,11 +1,14 @@
 let PHP_API = ""
 let NODE_API = ""
 
-if (process.env.VUE_APP_MODE == "production") {
+if (process.env.VUE_APP_MODE === "production") {
   PHP_API = "https://wx.anyteach.cn"
   NODE_API = "https://anyteach.cn/api"
-} else {
+} else if (process.env.VUE_APP_MODE === "staging") {
   PHP_API = "https://php-staging.anyteach.cn"
+  NODE_API = "https://node-staging.anyteach.cn/api"
+} else {
+  PHP_API = "http://localhost:8080/api"
   NODE_API = "https://node-staging.anyteach.cn/api"
 }
 
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
 
+
 module.exports = {
   devServer: {
-    // proxy: 'localhost:3000'
-    port:"3000",
+    port: 8080,
+    proxy: {
+      "^/api": {
+        target: "https://php-staging.anyteach.cn",
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "/"
+        }
+      },
+    }
   },
   pluginOptions: {
     electronBuilder: {
@@ -10,7 +20,7 @@ module.exports = {
         // options placed here will be merged with default configuration and passed to electron-builder
         productName: "AnyTeach",
         appId: "com.anyteach.desktop",
-        copyright: "版权所有 © 2020 厦门市睿博东方文化传媒有限公司",
+        copyright: `版权所有 © ${new Date().getUTCFullYear()} 厦门市睿博东方文化传媒有限公司`,
         dmg: {
           sign: false
         },
